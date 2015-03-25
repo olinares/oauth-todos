90a369f75f4c485640ec41e53036860444aa6842
olinares <olinares9833@gmail.com>
2015-03-25 12:36:35 -0700

    added sorcery & figaro env vars

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-  authenticates_with_sorcery!
+  authenticates_with_sorcery! do |config|
+    config.authentications_class = Authentication
+  end
+
+  has_many :authentications, :dependent => :destroy
+  accepts_nested_attributes_for :authentications
+
+  def has_linked_github?
+    authentications.where(provider: 'github').present?
+  end
 end
